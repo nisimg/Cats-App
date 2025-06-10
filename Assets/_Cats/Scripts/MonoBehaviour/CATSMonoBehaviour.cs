6197defea94920307ba3a8cf635819ea54d04f3b
NISIM <NISIM550@GMAIL.COM>
2025-06-10 21:12:26 +0300

    Creates base CATSMonoBehaviour class
    Introduces a base class for CATS-related MonoBehaviours.
    
    This provides a common interface for accessing the CATSManager,
    handling events, and managing updates. It simplifies the
    implementation of CATS-related components and promotes code reuse.

--- Assets/_Cats/Scripts/MonoBehaviour/CATSMonoBehaviour.cs
+++ Assets/_Cats/Scripts/MonoBehaviour/CATSMonoBehaviour.cs
@@ -1,7 +1,67 @@
+using System;
+using System.Collections;
 using _cats.Scripts.Core;
+using _cats.Scripts.Core._cats.Scripts.Core;
 using UnityEngine;
 
-public class CATSMonoBehaviour : MonoBehaviour
+public class CATSMonoBehaviour : MonoBehaviour, ICATSUpdatable
 {
     CATSManager _manager => CATSManager.Instance;
+
+    protected void AddListener(CATSEventNames eventName, Action<object> onGameStart) =>
+        _manager.EventsManager.AddListener(eventName, onGameStart);
+
+    protected void RemoveListener(CATSEventNames eventName, Action<object> onGameStart) =>
+        _manager.EventsManager.RemoveListener(eventName, onGameStart);
+
+    protected void InvokeEvent(CATSEventNames eventName, object obj) =>
+        _manager.EventsManager.InvokeEvent(eventName, obj);
+
+    public Coroutine WaitForSeconds(float time, Action onComplete)
+    {
+        return StartCoroutine(WaitForSecondsCoroutine(time, onComplete));
+    }
+
+    private IEnumerator WaitForSecondsCoroutine(float time, Action onComplete)
+    {
+        yield return new WaitForSeconds(time);
+        onComplete?.Invoke();
+    }
+
+    public Coroutine WaitForFrame(Action onComplete)
+    {
+        return StartCoroutine(WaitForFrameCoroutine(onComplete));
+    }
+
+    private IEnumerator WaitForFrameCoroutine(Action onComplete)
+    {
+        yield return null;
+        onComplete?.Invoke();
+    }
+
+    public void Start()
+    {
+        _manager.UpdateManager.RegisterUpdatable(this);
+    }
+
+    public virtual void OnUpdate(float deltaTime)
+    {
+    }
+
+    public virtual void OnLateUpdate(float deltaTime)
+    {
+    }
+
+    public virtual void OnFixedUpdate(float deltaTime)
+    {
+    }
+
+    public virtual void OnIntervalUpdate(float deltaTime)
+    {
+    }
+
+    public void OnDestroy()
+    {
+        _manager.UpdateManager.UnregisterUpdatable(this);
+    }
 }
